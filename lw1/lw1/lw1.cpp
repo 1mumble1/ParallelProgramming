04f3d6a48813b7c46652ba9657746098cebe9aad
1mumble1 <mrt47@yandex.ru>
2024-09-19 10:15:39 +0300

    Исправлено создание потоков

--- lw1/lw1/lw1.cpp
+++ lw1/lw1/lw1.cpp
@@ -1,25 +1,42 @@
+#include <windows.h>
+#include <string>
 #include <iostream>
-#include <thread>
-#include "Windows.h"
 
-void ThreadProc(int id)
+HANDLE mtx;
+
+DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
 {
-    std::cout << "Поток №" << id << " выполянет свою работу." << std::endl;
+	int* threadNumber = static_cast<int*>(lpParam);
+
+	WaitForSingleObject(mtx, INFINITE);
+	
+	std::cout << "Поток №" << *threadNumber << " выполняет свою работу." << std::endl;
+	
+	ReleaseMutex(mtx);
+	ExitThread(0); // функция устанавливает код завершения потока в 0
 }
 
 int main()
 {
-    SetConsoleOutputCP(1251);
+	SetConsoleOutputCP(1251);
 
-    std::cout << "Введите количество потоков: ";
-    int countOfThreads;
-    std::cin >> countOfThreads;
+	int countOfThreads;
+	std::cin >> countOfThreads;
+	
+	// создание потоков
+	HANDLE* handles = new HANDLE[countOfThreads];
+	int* threadNumbers = new int[countOfThreads];
 
-    for (int i = 1; i <= countOfThreads; i++)
-    {
-        std::thread thread(ThreadProc, i);
-        thread.join();
-    }
+	// Создаем мьютекс
+	mtx = CreateMutex(NULL, FALSE, NULL);
 
-    return 0;
+	for (int i = 0; i < countOfThreads; i++)
+	{
+		threadNumbers[i] = i + 1;
+		handles[i] = CreateThread(NULL, 0, &ThreadProc, &threadNumbers[i], 0, NULL);
+	}
+
+	// ожидание окончания работы потоков
+	WaitForMultipleObjects(countOfThreads, handles, true, INFINITE);
+	return 0;
 }
