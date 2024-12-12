6291dd717f0f24b0505f2f9e4127b407e5ed5984
1mumble1 <mrt47@yandex.ru>
2024-12-12 10:55:51 +0300

    lw7

---
+++ lw7/lw7_1/1.cs
@@ -0,0 +1,64 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Введите имя текстового файла: ");
+            string fileName = Console.ReadLine() ?? throw new Exception("Empty file name.");
+            string filePath = "C:\\MRT47\\ParallelProgramming\\lw7\\" + fileName;
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Файл не найден.");
+                return;
+            }
+
+            Console.WriteLine("Введите символы, которые нужно удалить (вводите их слитно, например, abc):");
+            string symbolsToRemove = Console.ReadLine();
+
+            string text = await ReadFileAsync(filePath);
+            string cleanedText = RemoveSymbols(text, symbolsToRemove);
+            await WriteFileAsync(filePath, cleanedText);
+            Console.WriteLine("Файл обновлен.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+
+    static async Task<string> ReadFileAsync(string filePath)
+    {
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            return await reader.ReadToEndAsync();
+        }
+    }
+
+    static string RemoveSymbols(string text, string? symbolsToRemove)
+    {
+        if (string.IsNullOrEmpty(symbolsToRemove))
+        {
+            return text;
+        }
+
+        string cleanedText = string.Empty;
+        foreach (char c in text)
+        {
+            if (!symbolsToRemove.Contains(c))
+            {
+                cleanedText += c;
+            }
+        }
+        return cleanedText;
+    }
+
+    static async Task WriteFileAsync(string filePath, string content)
+    {
+        using (StreamWriter writer = new StreamWriter(filePath, false))
+        {
+            await writer.WriteAsync(content);
+        }
+    }
+}
---
+++ lw7/lw7_2/2.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Download images");
+        const int numImages = 10;
+
+        //await DownloadImagesParallelAsync(numImages);
+        await DownloadImagesSequentAsync(numImages);
+    }
+
+    static async Task DownloadImagesParallelAsync(int numImages)
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        List<Task> tasks = [];
+        for (int i = 0; i < numImages; i++)
+        {
+            tasks.Add(DownloadImageAsync());
+        }
+
+        await Task.WhenAll(tasks);
+
+        sw.Stop();
+        Console.WriteLine($"Success! Time: {sw.ElapsedMilliseconds} ms");
+    }
+
+    static async Task DownloadImagesSequentAsync(int numImages)
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        for (int i = 0; i < numImages; i++)
+        {
+            await DownloadImageAsync();
+        }
+
+        sw.Stop();
+        Console.WriteLine($"Success! Time: {sw.ElapsedMilliseconds} ms");
+    }
+
+    static async Task DownloadImageAsync()
+    {
+        string urlAPI = "https://dog.ceo/api/breeds/image/random";
+
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(urlAPI);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    Response dogResponse = JsonSerializer.Deserialize<Response>(json);
+
+                    Console.WriteLine($"Starting download from url: {dogResponse.message}");
+
+                    if (dogResponse.status == "success")
+                    {
+                        Console.WriteLine($"Image from {dogResponse.message} download succesfully!");
+                    }
+                    else
+                    {
+                        throw new Exception($"{dogResponse.status}");
+                    }
+                }
+                else
+                {
+                    throw new Exception($"{response.StatusCode}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
+public class Response
+{
+    public string message { get; set; }
+    public string status { get; set; }
+}
